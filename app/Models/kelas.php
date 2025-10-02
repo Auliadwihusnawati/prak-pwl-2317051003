cae466cd05e728b9ca736ef45d91a3a55479f330
Auliadwihusnawati <akuaulia58@gmail.com>
2025-10-03 00:59:22 +0700

    praktikum 4

--- app/Models/kelas.php
+++ app/Models/kelas.php
@@ -15,4 +15,7 @@ class kelas extends Model
     {
         return $this->hasMany(UserModel::class, 'kelas_id');
     }
+    public function getKelas(){
+    return $this->all();
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpKernel\Profiler\Profile;
+use App\Http\Controllers\ProfileController;
+use APP\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,6 @@ Route::get('/', function () {
 });
 
 Route::get('/profile', [ProfileController::class, 'profile']);
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::post('/user', [UserController::class, 'store'])->name('user.store');
