611da4dfa32f740252e4f86a5908f70b4e6b69a5
Auliadwihusnawati <akuaulia58@gmail.com>
2025-10-02 20:13:26 +0700

    praktikum 4

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    public function profile(){
+
+        $data = [
+            'Nama' => 'Aulia Dwi Husnawati',
+            'NPM' => '2317051003',
+            'Kelas' => 'B'
+        ];
+    }
     //
 }
